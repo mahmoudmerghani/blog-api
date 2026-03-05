671c5fa67c85ff6ee93fb7a2a1bce5d1b66dd74f
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2026-03-05 17:15:51 +0200

    Change cors config

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,16 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import blogsRouter from "./routes/blogsRouter.js";
 import authRouter from "./routes/authRouter.js";
 import commentsRouter from "./routes/commentsRouter.js";
 
-
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.ALLOWED_ORIGIN,
+    }),
+);
 
 app.use(express.json());
 
@@ -17,12 +20,11 @@ app.use("/api/comments", commentsRouter);
 
 app.use((req, res) => {
     res.status(404).json({ error: "Not found" });
-})
+});
 
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).json({ error: "Server error" });
-})
-
+});
 
 app.listen(8080);
